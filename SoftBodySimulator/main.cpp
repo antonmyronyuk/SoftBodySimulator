cd19fe8c84d83f29e38429aac5ad2faebcf60ded
Anton Myronyuk <antonmyronyuk@gmail.com>
2017-12-17 12:59:55 +0200

    fixed cube movement while rotating the world

--- SoftBodySimulator/main.cpp
+++ SoftBodySimulator/main.cpp
@@ -1,5 +1,6 @@
 #include "SoftBody.h"
 #include <glut.h>
+#include <iostream>
 
 
 // world (cube)
@@ -8,6 +9,7 @@ float angleY = 0.f;
 float worldW = 200.f;
 float worldH = 200.f;
 float worldD = 200.f;
+float pi = 3.141592f;
 
 GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };		/* Red diffuse light. */
 GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };		/* Infinite light location. */
@@ -31,13 +33,15 @@ Point3d getOGLPos(int x, int y) {
 	winX = (float)x;
 	winY = (float)viewport[3] - (float)y;
 	glReadPixels(x, int(winY), 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &winZ);
-	//std::cout << winZ << std::endl;
+	std::cout << winZ << std::endl;
+	
 	if (winZ > 0.999999999f) {
 		winZ = 0.997f;
 	}
+	
 
 	gluUnProject(winX, winY, winZ, modelview, projection, viewport, &posX, &posY, &posZ);
-	//std::cout << posX << "   " << posY << "   " << posZ << std::endl;
+	std::cout << posX << "   " << posY << "   " << posZ << std::endl;
 	return Point3d(posX, posY, posZ);
 }
 
@@ -60,7 +64,7 @@ void init() {
 	glMatrixMode(GL_PROJECTION);
 	gluPerspective(40.f, 1.f, 1.f, 900.f);
 	glMatrixMode(GL_MODELVIEW);
-	gluLookAt(0.f, 0.f, 700.f,	//eye position
+	gluLookAt(0.f, 0.f, 500.f,	//eye position
 		0.f, 0.f, 0.f,			//center position
 		0.f, 1.f, 0.f);			//direction
 
@@ -82,6 +86,7 @@ void processRotateKeys(int key, int x, int y) {
 	else if (key == GLUT_KEY_DOWN) {
 		body.disableFix();
 	}
+	std::cout << angleY << std::endl;
 
 }
 
@@ -89,6 +94,7 @@ bool rightDown = false;
 bool leftDown = false;
 
 void mouseFunc(int button, int state, int x, int y) {
+	
 	if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON) {
 		leftDown = true;
 	}
@@ -106,23 +112,26 @@ void mouseFunc(int button, int state, int x, int y) {
 
 	if (leftDown) {
 		Point3d p = getOGLPos(x, y);
-		particlesSym.turnForceOn(p);
+		particlesSym.turnForceOn(Point3d(p.X() * cos(angleY * pi / 180) - p.Z() * sin(angleY * pi / 180),
+			p.Y(),
+			p.Z() * cos(angleY * pi / 180) + p.X() * sin(angleY * pi / 180))
+		);
 	}
+	/*
 	if (rightDown) {
 		Point3d p = getOGLPos(x, y);
 		body.addFix(0, p.X(), p.Y(), p.Z());
 	}
-
+	*/
 }
 
 void motionFunc(int x, int y) {
-	if (leftDown) {
-		Point3d p = getOGLPos(x, y);
-		particlesSym.turnForceOn(p);
-	}
+	
+	std::cout << "x = " << x
+		<< "y = " << y << std::endl;
 	if (rightDown) {
 		Point3d p = getOGLPos(x, y);
-		body.addFix(0, p.X(), p.Y(), p.Z());
+		body.addFix(0, 0.55f * p.X() * cos(angleY * pi / 180), 0.55f * p.Y(), 0.55f * p.X() * sin(angleY * pi / 180));
 	}
 }
 
@@ -238,7 +247,8 @@ void drawWorld() {
 
 	particlesSym.step(20);
 	glPushMatrix();
-	//glRotatef(angleY, 0.f, 1.f, 0.f);
+	glRotatef(angleY, 0.f, 1.f, 0.f);
+	//std::cout << sin(angleY) << std::endl;
 
 	glColor3f(0.f, 0.f, 0.f);
 	glDisable(GL_LIGHTING);
@@ -283,6 +293,7 @@ void drawWorld() {
 	glEnd();
 	glEnable(GL_LIGHTING);
 	body.renderCube();
+	//body.setPointPos(0, 0.f, 0.f, 0.f);
 	glPopMatrix();
 }
 
